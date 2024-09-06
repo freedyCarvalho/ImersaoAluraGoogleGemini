e446057e254399f0ae5750b0c6f8dffbff8c518a
Freedy Carvalho (Zaki) <freedy.carvalho@mk2.com.br>
2024-09-06 11:04:22 -0300

    Update animation.js
    Implementando a transição da header

--- js/animation.js
+++ js/animation.js
@@ -20,3 +20,25 @@ window.addEventListener('scroll', () => {
 
 // Adiciona um evento de clique ao botão para rolar para o topo
 backToTopButton.addEventListener('click', scrollToTop);
+
+// Transição da Header
+// Seleciona o elemento do header
+const header = document.querySelector('header');
+
+let indiceImagem = 0;
+
+function trocarImagem() {
+  // Atualiza a propriedade background-image do header
+  header.style.backgroundImage = `url(${imagens[indiceImagem]})`;
+
+  // Incrementa o índice da imagem
+  indiceImagem++;
+
+  // Se o índice ultrapassar o limite do array, volta para o início
+  if (indiceImagem >= imagens.length) {
+      indiceImagem = 0;
+  }
+}
+
+// Chama a função trocarImagem a cada 5 segundos
+setInterval(trocarImagem, 5000);
