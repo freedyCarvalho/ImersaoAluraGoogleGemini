f4c338fbcdb6a695795fbd51f6f0613099dc3346
Freedy Carvalho <fhamzagic@sinaf.com.br>
2024-09-06 14:47:05 -0300

    Implementando o addEventListener para carregar os scripts

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,28 @@
+// // Seleciona o botão
+// const backToTopButton = document.querySelector('.back-to-top');
+
+
+// // Função para rolar suavemente para o topo
+// function scrollToTop() {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth'
+//   });
+// }
+
+// window.addEventListener('scroll', () => {
+//     if (window.pageYOffset > 100) {
+//       backToTopButton.classList.add('show');
+//     } else {
+//       backToTopButton.classList.remove('show');   
+  
+//     }
+//   });
+
+// // Adiciona um evento de clique ao botão para rolar para o topo
+// backToTopButton.addEventListener('click', scrollToTop);
+
+
 // Seleciona o botão
 const backToTopButton = document.querySelector('.back-to-top');
 
@@ -10,16 +35,20 @@ function scrollToTop() {
 }
 
 window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 100) {
+    if (window.scrollY > 100) {
       backToTopButton.classList.add('show');
     } else {
-      backToTopButton.classList.remove('show');   
-  
+      backToTopButton.classList.remove('show');
     }
-  });
+});
 
 // Adiciona um evento de clique ao botão para rolar para o topo
 backToTopButton.addEventListener('click', scrollToTop);
+
+
+
+
+
 
 
 /*
