78c2021c343f702ce0d4b7a46ec2600939e68655
Freedy Carvalho <fhamzagic@sinaf.com.br>
2024-09-06 11:01:25 -0300

    Implementando transição da header

--- js/animation.js
+++ js/animation.js
@@ -20,3 +20,62 @@ window.addEventListener('scroll', () => {
 
 // Adiciona um evento de clique ao botão para rolar para o topo
 backToTopButton.addEventListener('click', scrollToTop);
+
+
+/*
+// Array com os caminhos das imagens
+const imagens = [
+  '../img/header-gemini-teia-shadow-d-opc.png.jpg',
+  '../img/header-homem-ia.png'
+];
+
+
+// Seleciona o elemento da imagem no HTML (certifique-se de ter um elemento com o id "imagemHeader")
+const imagemHeader = document.getElementById('imagemHeader');
+//const imagemHeader = document.getElementsByTagName('header');
+
+let indiceImagem = 0;
+
+function trocarImagem() {
+  // Atualiza o atributo src da imagem
+  imagemHeader.src = imagens[indiceImagem];
+
+  // Incrementa o índice da imagem
+  indiceImagem++;
+
+  // Se o índice ultrapassar o limite do array, volta para o início
+  if (indiceImagem >= imagens.length) {
+      indiceImagem = 0;
+  }
+}
+
+// Chama a função trocarImagem a cada 5 segundos
+setInterval(trocarImagem, 5000);
+*/
+
+// Array com os caminhos das imagens
+const imagens = [
+  '../img/header-gemini-teia-shadow-d-opc.png',
+  '../img/header-homem-ia.png'
+];
+
+// Seleciona o elemento do header
+const header = document.querySelector('header');
+
+let indiceImagem = 0;
+
+function trocarImagem() {
+  // Atualiza a propriedade background-image do header
+  header.style.backgroundImage = `url(${imagens[indiceImagem]})`;
+
+  // Incrementa o índice da imagem
+  indiceImagem++;
+
+  // Se o índice ultrapassar o limite do array, volta para o início
+  if (indiceImagem >= imagens.length) {
+      indiceImagem = 0;
+  }
+}
+
+// Chama a função trocarImagem a cada 5 segundos
+setInterval(trocarImagem, 5000);
